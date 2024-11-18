fc5b1b0c2b43d448c8d65750f8314b28ee679971
douglasvdev <douglasv.dev@gmail.com>
2024-11-17 22:44:13 -0300

    starting_project

---
+++ src/components/category/index.tsx
@@ -0,0 +1,19 @@
+import {Text, Pressable, PressableProps } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+type Props = PressableProps & {
+    name: string,
+    icon: keyof typeof MaterialIcons.glyphMap
+}
+
+export function Category({name, icon, ...rest}: Props) {
+    return (
+        <Pressable style={styles.container} {...rest}>
+            <MaterialIcons name={icon} size={16} color={colors.gray[400]} />
+            <Text style={styles.name}>{name}</Text>
+        </Pressable>
+    )
+}
