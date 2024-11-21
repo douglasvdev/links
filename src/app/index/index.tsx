67a0382e921f1e26a4f2f6428fa1b172dec2f87e
douglasvdev <douglasv.dev@gmail.com>
2024-11-21 00:40:28 -0300

    Modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,15 @@
-import { Image, TouchableOpacity, View } from "react-native"
+import { FlatList, Image, Modal, Text, TouchableOpacity, View } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 
-import { Category } from "@/components/category"
+import { Link } from "@/components/link"
+import { Categories } from "@/components/categories"
+import { Option } from "@/components/option"
+
+// import { Category } from "@/components/category"
+// import { categories } from "@/utils/categories"
 
 export default function Index(){
     return (
@@ -16,10 +21,46 @@ export default function Index(){
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
             </View>
+
+            <Categories />
+
             
-            <Category name="projetos" icon="code"/>
-            <Category name="Site" icon="language"/>
-            <Category name="Video" icon="movie"/>
+            <FlatList 
+                data={["1", "2", "3", "4"]} 
+                keyExtractor={(item) => item} 
+                renderItem={() => (
+                    <Link name="RocketSeat" url="https://www.rocketseat.com.br/" onDetails={() => console.log("Clicou!!")}/>
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
+
+            <Modal transparent visible={false}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>Curso</Text>
+
+                            <TouchableOpacity>
+                                <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text style={styles.modalLinkName}>RocketSeat</Text>
+                        <Text style={styles.modalLinkUrl}>https://www.rocketseat.com.br/discover</Text>
+
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secundary" />
+                            <Option name="Abrir" icon="language" variant="primary" />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            {/* <Category name={categories[5].name} icon={categories[5].icon} isSelected/>
+            <Category name="Sites" icon="language" isSelected={false}/>
+            <Category name="Videos" icon="movie" isSelected={false}/> */}
 
         </View>
     )
